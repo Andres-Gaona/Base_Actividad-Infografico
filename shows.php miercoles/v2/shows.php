08aaa8357d2deb7abdabbc21f6b512bfeaf18d48
Andres-Gaona <147674998+Andres-Gaona@users.noreply.github.com>
2026-01-07 23:47:29 -0600

    Add files via upload

---
+++ shows.php miercoles/v2/shows.php
@@ -0,0 +1,95 @@
+ 
+<!doctype html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>SHOW MIERCOLES</title>
+<meta name="robots" content="noindex, nofollow">
+<link rel="shortcut icon" href="../assets/icons/favicon.ico" />
+<link rel="apple-touch-icon" href="../assets/icons/apple-touch-icon.png" />
+<link rel="apple-touch-icon" sizes="72x72" href="../assets/icons/apple-touch-icon-72x72.png" />
+<link rel="apple-touch-icon" sizes="114x114" href="../assets/icons/apple-touch-icon-114x114.png" />
+    <link href="css/reset.css" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,600,800|Roboto+Slab:400,700" rel="stylesheet">
+    <link href="css/fonts.css" rel="stylesheet" type="text/css">
+    <link href="css/layout.css" rel="stylesheet" type="text/css">
+    
+    <link href="../css/weather-icons.css" rel="stylesheet" type="text/css">
+    <link href="../css/preloader.css" rel="stylesheet" type="text/css">
+
+
+
+     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+		  <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+		  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+		<![endif]-->
+
+
+
+    <script src="../js/jquery-1.11.3.min.js"></script>
+
+
+
+    <script src="../js/carousellite/lib/jquery.easing-1.3.js"></script>
+    <script src="../js/carousellite/lib/jquery.easing.compatibility.js"></script>
+    <script src="../js/carousellite/lib/jquery.mousewheel-3.1.12.js"></script>
+    <script src="../js/carousellite/src/jquery.jcarousellite.js"></script>
+</head>
+
+<body>
+
+
+
+  
+
+    
+
+    <div class="show_rotator_box" style="background-image:url(../assets/activities/2838023a778dfaecdc212708f721b788.jpg); background-position: center center">
+
+
+        <div class="day_tittle_box">
+            <div class="day_tittle_content"><span class="normal_font">MIERCOLES</span> <span class="bold_font"> / WEDNESDAY</span>
+            </div>
+        </div>
+
+        <div class="shadow_content"></div>
+
+
+
+
+        <div class="content_show">
+            <div class="show_name">
+                <div class="show_tittle">Tropical</div>
+                            </div>
+            <div class="show_info">
+                <div class="place_show">
+                    <span class="icon icon-pointer"></span>
+                    <div>Teatro Oaxaca </div>
+                    <div class="show_place_en">Oaxaca Theater</div>
+                </div>
+
+                <div class="hour_show"><span class="icon icon-clock"></span></span>
+                  9:00 PM</div>
+
+                <div class="type_show">
+                    <span class="icon icon-person"></span> <span class="orange_text">Familiar</span> - <span class="gray_text">Family</span>
+                </div>
+                <div class="position_show">
+                    <div class="orange_flag">SHOW</div>
+                </div>
+            </div>
+
+
+        </div>
+
+
+    </div>
+
+
+
+
+
+</body>
+</html>
