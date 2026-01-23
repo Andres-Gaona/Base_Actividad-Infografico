2346ce07cc696384a91aa4515859a0309e40f810
Andres-Gaona <147674998+Andres-Gaona@users.noreply.github.com>
2026-01-23 17:55:39 -0600

    Add files via upload

--- js/carousellite/lib/jquery.easing.compatibility.js
+++ js/carousellite/lib/jquery.easing.compatibility.js
@@ -1,8 +1,11 @@
 /*
- * jQuery Easing Compatibility v1
+ * Easing Compatibility - http://gsgd.co.uk/sandbox/jquery/easing
  *
  * Adds compatibility for applications that use the pre 1.2 easing names
  *
+ * Copyright (c) 2007 George Smith
+ * Licensed under the MIT License:
+ *   http://www.opensource.org/licenses/mit-license.php
  */
 
 jQuery.extend( jQuery.easing,
