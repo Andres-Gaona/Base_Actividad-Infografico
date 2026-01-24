1b0b8c7c401660d5e3bacaa1dbcc9628a43b6309
Andres-Gaona <147674998+Andres-Gaona@users.noreply.github.com>
2026-01-23 19:01:38 -0600

    Add files via upload

---
+++ shows.php/shows.php lunes/PanInfo/shows.php
@@ -0,0 +1,151 @@
+ 
+<!doctype html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>SHOW LUNES</title>
+<meta name="robots" content="noindex, nofollow">
+<link rel="shortcut icon" href="" />
+    <link href="css/reset.css" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,600,800|Roboto+Slab:400,700" rel="stylesheet">
+    <link href="css/fonts.css" rel="stylesheet" type="text/css">
+    <link href="css/layout.css" rel="stylesheet" type="text/css">
+    
+    <link href="../css/weather-icons.css" rel="stylesheet" type="text/css">
+    <link href="../css/preloader.css" rel="stylesheet" type="text/css">
+
+    <script src="../js/jquery.min.js"></script>
+
+
+
+    <script src="../js/carousellite/lib/jquery.easing.js"></script>
+    <script src="../js/carousellite/lib/jquery.easing.compatibility.js"></script>
+    <script src="../js/carousellite/lib/jquery.mousewheel.js"></script>
+    <script src="../js/carousellite/src/jquery.jcarousellite.js"></script>
+</head>
+
+<body>
+
+    <div class="show_rotator_box" style="background-image:url(../assets/activities/d82c8d1619ad8176d665453cfb2e55f0.jpg); background-position: center center">
+        <div class="day_tittle_box">
+            <div class="day_tittle_content_blue"><span class="normal_font">LUNES</span> <span class="bold_font"> / MONDAY</span>
+            </div>
+        </div>
+
+        <div class="shadow_content"></div>
+
+        <div class="content_show_week">
+            <div class="show_week_box">
+                <div class="tittle_show_week">Leyendas</div>
+                <div class="tittle_show_week_info">
+                    <div class="info_place_week">
+                        <span class="icon icon-pointer place_pointer_week"></span>
+                        <div class="week_show_place inline_block">Teatro Oaxaca  <span class="orange_text"> / Oaxaca Theater</span></div>
+
+
+                    </div>
+                    <div><span class="icon icon-clock clock_show_week"></span>
+                        <div class="week_show_time inline_block "> 9:00 PM</div>
+                    </div>
+                    <div class="orange_flag_week ">SHOW</div>
+
+                </div>
+            </div>
+            <div id="rest_rotator_week">
+                <ul>
+                                        <li>
+                        <div>
+                          <div class="time_rest_week">
+                            
+                            <div class="restaurant_name_week">
+                              <div class="circle_fork"> <span class="icon icon-fork fork_week"></span>
+                                </div>
+                              <div>La Fontana</div>
+                              </div>
+                            <div class="restaurant_time_week"><span class="icon icon-clock clock_show_week"></span>
+                              <div class="time_content_rest_week">6:00 PM </div>
+                              <div class="inline_block">Italiano <span class="orange_text">/ Italian</span> </div>
+                              </div>
+                            
+                            
+                            </div>
+                          <div class="border_show_week"></div>
+                          </div>
+                      </li>
+                                          <li>
+                        <div>
+                          <div class="time_rest_week">
+                            
+                            <div class="restaurant_name_week">
+                              <div class="circle_fork"> <span class="icon icon-fork fork_week"></span>
+                                </div>
+                              <div>El Agave</div>
+                              </div>
+                            <div class="restaurant_time_week"><span class="icon icon-clock clock_show_week"></span>
+                              <div class="time_content_rest_week">6:00 PM </div>
+                              <div class="inline_block">Mexicano <span class="orange_text">/ Mexican</span> </div>
+                              </div>
+                            
+                            
+                            </div>
+                          <div class="border_show_week"></div>
+                          </div>
+                      </li>
+                                          <li>
+                        <div>
+                          <div class="time_rest_week">
+                            
+                            <div class="restaurant_name_week">
+                              <div class="circle_fork"> <span class="icon icon-fork fork_week"></span>
+                                </div>
+                              <div>La Tortuga</div>
+                              </div>
+                            <div class="restaurant_time_week"><span class="icon icon-clock clock_show_week"></span>
+                              <div class="time_content_rest_week">7:30 PM </div>
+                              <div class="inline_block">Carnes y Mariscos <span class="orange_text">/ Meat cuts and Seafood</span> </div>
+                              </div>
+                            
+                            
+                            </div>
+                          <div class="border_show_week"></div>
+                          </div>
+                      </li>
+                                          <li>
+                        <div>
+                          <div class="time_rest_week">
+                            
+                            <div class="restaurant_name_week">
+                              <div class="circle_fork"> <span class="icon icon-fork fork_week"></span>
+                                </div>
+                              <div>Kyoto</div>
+                              </div>
+                            <div class="restaurant_time_week"><span class="icon icon-clock clock_show_week"></span>
+                              <div class="time_content_rest_week">6:00 PM </div>
+                              <div class="inline_block">Sushi Bar <span class="orange_text">/ Sushi Bar</span> </div>
+                              </div>
+                            
+                            
+                            </div>
+                          <div class="border_show_week"></div>
+                          </div>
+                      </li>
+                      
+                </ul>
+            </div>
+        </div>
+    </div>
+
+        <script type="text/javascript">
+        $( "#rest_rotator_week" ).jCarouselLite( {
+
+            vertical: true,
+            auto: 6000,
+            start: 0,
+            scroll: 1,
+            speed: 4000,
+            visible: 8
+        } );
+    </script>
+
+</body>
+</html>
---
+++ shows.php/shows.php miercoles/PanInfo/shows.php
@@ -0,0 +1,66 @@
+ 
+<!doctype html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>SHOW MIERCOLES</title>
+    <meta name="robots" content="noindex, nofollow">
+    <link rel="shortcut icon" href="" />
+    <link href="css/reset.css" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,600,800|Roboto+Slab:400,700" rel="stylesheet">
+    <link href="css/fonts.css" rel="stylesheet" type="text/css">
+    <link href="css/layout.css" rel="stylesheet" type="text/css">
+    
+    <link href="../css/weather-icons.css" rel="stylesheet" type="text/css">
+    <link href="../css/preloader.css" rel="stylesheet" type="text/css">
+
+    <script src="../js/jquery.min.js"></script>
+
+    <script src="../js/carousellite/lib/jquery.easing.js"></script>
+    <script src="../js/carousellite/lib/jquery.easing.compatibility.js"></script>
+    <script src="../js/carousellite/lib/jquery.mousewheel.js"></script>
+    <script src="../js/carousellite/src/jquery.jcarousellite.js"></script>
+</head>
+
+<body>
+
+    <div class="show_rotator_box" style="background-image:url(../assets/activities/2838023a778dfaecdc212708f721b788.jpg); background-position: center center">
+
+
+        <div class="day_tittle_box">
+            <div class="day_tittle_content"><span class="normal_font">MIERCOLES</span> <span class="bold_font"> / WEDNESDAY</span>
+            </div>
+        </div>
+
+        <div class="shadow_content"></div>
+
+        <div class="content_show">
+            <div class="show_name">
+                <div class="show_tittle">Tropical</div>
+                            </div>
+            <div class="show_info">
+                <div class="place_show">
+                    <span class="icon icon-pointer"></span>
+                    <div>Teatro Oaxaca </div>
+                    <div class="show_place_en">Oaxaca Theater</div>
+                </div>
+
+                <div class="hour_show"><span class="icon icon-clock"></span></span>
+                  9:00 PM</div>
+
+                <div class="type_show">
+                    <span class="icon icon-person"></span> <span class="orange_text">Familiar</span> - <span class="gray_text">Family</span>
+                </div>
+                <div class="position_show">
+                    <div class="orange_flag">SHOW</div>
+                </div>
+            </div>
+
+
+        </div>
+
+
+    </div>
+
+</body>
+</html>
